fb3103acd93642a54ee2e54b609e41106b6ebfc5
LBergen <lars.derek.bergen@gmail.com>
2018-08-09 19:04:56 -0700

    g

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("baconator", false);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Bobby's barbeque burger", true);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("big burger", true);
